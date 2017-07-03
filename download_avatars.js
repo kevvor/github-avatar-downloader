69118be70a4244b731b71d179513c0efcbc6362e
keano voelkl <keanov@gmail.com>
2017-07-03 22:11:42 +0000

    can download avatar images from urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = 'kevvor';
 var GITHUB_TOKEN = '49f5acd51a788ba6ba32d52770a728be0c348f21';
@@ -7,12 +8,49 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var requestURL = 'https://' + GITHUB_USER + ':' + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'kevvor'
+    }
+  };
 
+  request(options, function(err, response, body) {
+    if (err) {
+      console.log('Error');
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      var json = JSON.parse(body);
+      cb(json);
+    }
+  });
 }
 
-getRepoContributors('jquery', 'jquery', function (err, result) {
-  console.log('Errors:', err);
-  console.log('Result:', result);
-});
+function printURLS(jsonObject) {
+  for (var i = 0; i < jsonObject.length; i++) {
+    var img_url = jsonObject[i].avatar_url;
+    downloadImagesByURL(img_url, `./${jsonObject[i]['login']}.jpg`)
+  }
+}
+
+function downloadImagesByURL (url, filepath) {
+  var options = {
+    url: url,
+    headers: {
+      'User-Agent': 'kevvor'
+    }
+  };
+  request.get(options)
+    .on('response', function(response) {
+      response.pipe(fs.createWriteStream(filepath));
+    })
+}
+
+getRepoContributors('jquery', 'jquery', printURLS);
+
+
+
+
